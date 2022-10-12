44cbe5ffbd72682314c25e43ab636b06196b3ace
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-12 21:47:29 +0700

    Menghubungkan MySQL dengan API (PHP)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(new MaterialApp(
@@ -17,15 +18,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-Future<List> ambilData() async{
-    
-}
+  Future<List> ambilData() async {
+    final response =
+        await http.get(Uri.parse("http://10.0.2.2/my_app_crud/ambildata.php"));
+    return json.decode(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Data Santri Lomba FASI"),),
+      appBar: AppBar(
+        title: Text("Data Santri Lomba FASI"),
+      ),
+      body: FutureBuilder<List>(
+        future: ambilData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? new ItemList(
+                  list: snapshot.data,
+                )
+              : new Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  
+  final List list;
+  ItemList({this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: list == null ? 0 : list.length,
+        itemBuilder: (context, i) {
+          return new Text(list[i]['nama_santri']);
+        }
     );
   }
 }
