52f52708a895fcf308911d5ad69d14adab54fe03
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-13 10:09:49 +0700

    Buat file identitas & tampil identitas santri

---
+++ lib/identitas.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Identitas extends StatefulWidget {
+  List list;
+  int index;
+  Identitas({this.index, this.list})
+
+  @override
+  State<Identitas> createState() => _IdentitasState();
+}
+
+class _IdentitasState extends State<Identitas> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("${widget.list[widget.index]['nama_santri']}")),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Card(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(widget.list[widget.index]['nama_santri'], style: TextStyle(fontSize: 15),),
+                Text("Nomor Santri : ${widget.list[widget.index]['nama_santri']}", style: TextStyle(fontSize: 15),),
+                Text("Asal : ${widget.list[widget.index]['nama_santri']}", style: TextStyle(fontSize: 15),),
+                Text("Nomor Telepon : ${widget.list[widget.index]['nama_santri']}", style: TextStyle(fontSize: 15),),
+                Padding(padding: const EdgeInsets.all(10),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
