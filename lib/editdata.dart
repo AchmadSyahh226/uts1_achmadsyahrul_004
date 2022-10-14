4f7e2c5c19e8e91262f5c02d6098a30e94ef06c7
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-15 06:17:11 +0700

    Tambah url, controller, & perbaiki btn

--- lib/editdata.dart
+++ lib/editdata.dart
@@ -1,32 +1,44 @@
+import 'package:crud_mobile_004_achmadsr/main.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './main.dart';
 
 class EditData extends StatefulWidget {
-
   final List list;
   final int index;
-  EditData({required this.list,required this.index});
+  EditData({required this.list, required this.index});
 
   @override
   State<EditData> createState() => _EditDataState();
 }
 
 class _EditDataState extends State<EditData> {
-
   late TextEditingController controllerNomor;
   late TextEditingController controllerNama;
   late TextEditingController controllerAsal;
   late TextEditingController controllerNotelp;
 
-void editData(){
-  
-}
+  void editData() {
+    var url = "http://10.0.2.2/my_crud_app/editdata.php";
 
-@override
+    http.post(Uri.parse(url), body: {
+      "nosantri": controllerNomor.text,
+      "namasantri": controllerNama.text,
+      "asalsantri": controllerAsal.text,
+      "notelpsantri": controllerNotelp.text,
+    });
+  }
+
+  @override
   void initState() {
-    controllerNomor= new TextEditingController(text: widget.list[widget.index]['no_santri'] );
-    controllerNama= new TextEditingController(text: widget.list[widget.index]['nama_santri'] );
-    controllerAsal= new TextEditingController(text: widget.list[widget.index]['asal_santri'] );
-    controllerNotelp= new TextEditingController(text: widget.list[widget.index]['no_telp_santri'] );
+    controllerNomor =
+        new TextEditingController(text: widget.list[widget.index]['no_santri']);
+    controllerNama = new TextEditingController(
+        text: widget.list[widget.index]['nama_santri']);
+    controllerAsal = new TextEditingController(
+        text: widget.list[widget.index]['asal_santri']);
+    controllerNotelp = new TextEditingController(
+        text: widget.list[widget.index]['no_telp_santri']);
     super.initState();
   }
 
@@ -70,10 +82,14 @@ void editData(){
               Padding(
                 padding: const EdgeInsets.all(10.0),
               ),
-              ElevatedButton(onPressed: () {
-                editData();
-                Navigator.pop(context);
-              }, child: Text("EDIT DATA",)),
+              ElevatedButton(
+                  onPressed: () {
+                    editData();
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "EDIT DATA",
+                  )),
             ]),
           ],
         ),
