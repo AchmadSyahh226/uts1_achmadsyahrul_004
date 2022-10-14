d1d34467eb9eb7bc973c7b2fe4c00028f50c0e6c
Achmad Syahrul Ramadhan <achmadsyahrul.21004@mhs.unesa.ac.id>
2022-10-14 07:55:41 +0700

    Tambah dan mengatur textfield

--- lib/tambahdata.dart
+++ lib/tambahdata.dart
@@ -12,6 +12,42 @@ class _TambahDataState extends State<TambahData> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Tambah Data")),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>
+            [Column(children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Nomor Peserta",
+                  label: Text("Nomor Peserta"),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Nama Peserta",
+                  label: Text("Nama Peserta"),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Asal Peserta",
+                  label: Text("Asal Peserta"),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Nomor Telepon Peserta",
+                  label: Text("Nomor Telepon Peserta"),
+                ),
+              ),
+              Padding(padding: const EdgeInsets.all(10.0),),
+        
+              ElevatedButton(onPressed: () {}, child: Text("TAMBAH DATA"))
+            ]),
+          ],
+        ),
+      ),
     );
   }
 }
